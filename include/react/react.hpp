9169cdbbc91751972739c1b61df42176541efde3
Louis Dionne <ldionne.2@gmail.com>
2013-07-01 17:59:08 -0400

    Add master header.

---
+++ include/react/react.hpp
@@ -0,0 +1,11 @@
+/*!
+ * @file
+ * This file includes the whole __react__ library.
+ */
+
+#ifndef REACT_REACT_HPP
+#define REACT_REACT_HPP
+
+
+
+#endif // !REACT_REACT_HPP
---
+++ test/master_header.cpp
@@ -0,0 +1,9 @@
+/*!
+ * @file
+ * This file tests the inclusion of the master header of the library.
+ */
+
+#include <react/react.hpp>
+
+
+int main() { }
