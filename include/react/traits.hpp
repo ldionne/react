9fb75d5b4ac6bf763f586a1ddc70225d5deb97ef
Louis Dionne <ldionne.2@gmail.com>
2013-07-11 19:55:54 -0400

    Make the traits SFINAE friendly.

--- include/react/traits.hpp
+++ include/react/traits.hpp
@@ -6,21 +6,55 @@
 #ifndef REACT_TRAITS_HPP
 #define REACT_TRAITS_HPP
 
+#include <boost/mpl/has_xxx.hpp>
+#include <boost/utility/enable_if.hpp>
+
+
 namespace react {
-    template <typename Computation>
-    struct dependencies_of {
-        using type = typename Computation::dependencies;
-    };
+    namespace traits_detail {
+        BOOST_MPL_HAS_XXX_TRAIT_DEF(dependencies)
+        BOOST_MPL_HAS_XXX_TRAIT_DEF(feature)
+        BOOST_MPL_HAS_XXX_TRAIT_DEF(subfeatures)
+
+        template <typename Computation>
+        struct dependencies_of {
+            using type = typename Computation::dependencies;
+        };
+
+        template <typename Computation>
+        struct feature_of {
+            using type = typename Computation::feature;
+        };
+
+        template <typename CompositeFeature>
+        struct subfeatures_of {
+            using type = typename CompositeFeature::subfeatures;
+        };
+    } // end namespace traits_detail
 
     template <typename Computation>
-    struct feature_of {
-        using type = typename Computation::feature;
-    };
+    struct dependencies_of
+        : boost::lazy_enable_if<
+            traits_detail::has_dependencies<Computation>,
+            traits_detail::dependencies_of<Computation>
+        >
+    { };
 
-    template <typename CompositeFeature>
-    struct subfeatures_of {
-        using type = typename CompositeFeature::subfeatures;
-    };
+    template <typename Computation>
+    struct feature_of
+        : boost::lazy_enable_if<
+            traits_detail::has_feature<Computation>,
+            traits_detail::feature_of<Computation>
+        >
+    { };
+
+    template <typename Computation>
+    struct subfeatures_of
+        : boost::lazy_enable_if<
+            traits_detail::has_subfeatures<Computation>,
+            traits_detail::subfeatures_of<Computation>
+        >
+    { };
 } // end namespace react
 
 #endif // !REACT_TRAITS_HPP
