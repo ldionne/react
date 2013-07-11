151e8f822a3cce66da3da8947574eadbd6cc372c
Louis Dionne <ldionne.2@gmail.com>
2013-07-11 14:17:10 -0400

    Update mpl_extensions and refactor the feature_dependency_graph.

--- include/react/detail/feature_dependency_graph.hpp
+++ include/react/detail/feature_dependency_graph.hpp
@@ -6,7 +6,6 @@
 #ifndef REACT_DETAIL_FEATURE_DEPENDENCY_GRAPH_HPP
 #define REACT_DETAIL_FEATURE_DEPENDENCY_GRAPH_HPP
 
-#include <react/archetypes.hpp>
 #include <react/traits.hpp>
 
 #include <boost/mpl/apply.hpp>
@@ -15,7 +14,7 @@
 #include <boost/mpl/placeholders.hpp>
 #include <boost/mpl/reachable_set.hpp>
 #include <boost/mpl/set.hpp>
-#include <boost/mpl/transform_view.hpp>
+#include <boost/mpl/set_insert_range.hpp>
 #include <boost/mpl/transform_view.hpp>
 
 
@@ -36,29 +35,31 @@ namespace feature_dependency_graph_detail {
 
         //! A vertex in the feature dependency graph represents a computation
         //! to be implemented in a feature set.
-        template <typename Computation>
+        template <typename Feature>
         struct vertex {
-            using computation = Computation;
+            using computation = typename computation_of<Feature>::type;
 
-            using adjacent_vertices = mpl::transform_view<
-                typename dependencies_of<Computation>::type,
-                vertex<computation_of<mpl::_1>>
+            using open_neighborhood = mpl::transform_view<
+                typename dependencies_of<computation>::type,
+                vertex<mpl::_1>
             >;
         };
 
-        struct pseudo_root_computation : incremental_computation_archetype<> {
-            using dependencies = typename mpl::fold<
-                TopLevelComputations,
-                mpl::set<>,
-                mpl::insert<mpl::_1, feature_of<mpl::_2>>
-            >::type;
-        };
-
-        using root_vertex = vertex<pseudo_root_computation>;
+        using TopLevelVertices = typename mpl::fold<
+            TopLevelComputations,
+            mpl::set<>,
+            mpl::insert<mpl::_1, vertex<feature_of<mpl::_2>>>
+        >::type;
 
     public:
         //! Set of all the vertices in the feature dependency graph.
-        using vertices = typename mpl::reachable_set<root_vertex>::type;
+        using vertices = typename mpl::fold<
+            TopLevelVertices,
+            TopLevelVertices,
+            mpl::set_insert_range<
+                mpl::_1, mpl::reachable_set<mpl::_2>
+            >
+        >::type;
     };
 } // end namespace feature_dependency_graph_detail
 
