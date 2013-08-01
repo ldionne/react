a4aa6651ac3d63ac1d978d057ff62653b8f1e9d1
Louis Dionne <ldionne.2@gmail.com>
2013-08-01 15:38:55 -0400

    Rethink the dependency_graph.. again.

--- include/react/detail/dependency_graph.hpp
+++ include/react/detail/dependency_graph.hpp
@@ -9,98 +9,99 @@
 #include <react/computations/depends_on.hpp>
 #include <react/traits.hpp>
 
+#include <boost/mpl/apply_wrap.hpp>
 #include <boost/mpl/assert.hpp>
 #include <boost/mpl/at.hpp>
 #include <boost/mpl/erase_key.hpp>
-#include <boost/mpl/graph_intrinsics.hpp>
 #include <boost/mpl/has_key.hpp>
+#include <boost/mpl/lambda.hpp>
 #include <boost/mpl/make_index_of.hpp>
 #include <boost/mpl/placeholders.hpp>
 #include <boost/mpl/reachable_set.hpp>
+#include <boost/mpl/tag_of.hpp>
 #include <boost/mpl/transform_view.hpp>
 
 
 namespace react { namespace detail {
-struct dependency_graph_tag;
+namespace dependency_graph_detail {
+    template <typename Computations>
+    struct default_getter {
+        using IndexedByName = typename boost::mpl::make_index_of<
+            Computations, name_of<boost::mpl::_1>
+        >::type;
+
+        template <typename Name>
+        struct apply : boost::mpl::at<IndexedByName, Name> {
+            static constexpr bool found = boost::mpl::has_key<
+                IndexedByName, Name
+            >::value;
+
+            BOOST_MPL_ASSERT_MSG(
+                found,
+unable_to_find_the_computation_associated_to_some_name_in_the_dependency_graph,
+                (Name, IndexedByName)
+            );
+
+            static_assert(found,
+            "unable to find the computation associated to "
+            "some name in the dependency graph ");
+        };
+    };
+} // end namespace dependency_graph_detail
 
 /*!
  * Type representing a compile-time graph of the dependencies between
  * computations.
  *
- * The `dependency_graph` also supports `boost::mpl::at`, which maps any
- * computation name to the `Computation` associated to it in the graph.
- *
  * @tparam Computations
- *         A Boost.MPL `ForwardSequence` of `Computation`s.
+ *         A Boost.MPL `ForwardSequence` of computations whose
+ *         dependencies shall generate the graph.
+ *
+ * @tparam GetComputation
+ *         A Boost.MPL `LambdaExpression` returning the computation
+ *         associated to the computation name (an arbitrary type) it is
+ *         invoked with. It defaults to fetching the result in a map `M`
+ *         such that `boost::mpl::at<M, name_of<C>::type>::type` is `C`
+ *         for every `C` in `Computations`.
  */
-template <typename Computations>
-struct dependency_graph {
-    // for graph intrinsics
-    struct mpl { using tag = dependency_graph_tag; };
-
-    // for mpl::sequence_tag required by mpl::at
-    using tag = typename mpl::tag;
-
-private:
+template <
+    typename Computations,
+    typename GetComputation =
+                        dependency_graph_detail::default_getter<Computations>
+>
+class dependency_graph {
     using ComputationNames = boost::mpl::transform_view<
         Computations, name_of<boost::mpl::_1>
     >;
-    using Root = computations::depends_on<ComputationNames>;
-
-    using ComputationsByName = typename boost::mpl::make_index_of<
-        Computations, name_of<boost::mpl::_1>
-    >::type;
-
-    friend boost::mpl::at_impl<dependency_graph_tag>;
-    template <typename Name>
-    class at : public boost::mpl::at<ComputationsByName, Name> {
-        static constexpr bool found = boost::mpl::has_key<
-            ComputationsByName, Name
-        >::value;
-
-        BOOST_MPL_ASSERT_MSG(
-            found,
-            unable_to_find_some_computation_in_the_set_of_computations_spanning_the_dependency_graph,
-            (Name, ComputationsByName)
-        );
-
-        static_assert(found,
-        "unable to find some computation in the set of computations "
-        "spanning the dependency graph");
-    };
+    using FakeRoot = computations::depends_on<ComputationNames>;
 
 public:
     using vertices = typename boost::mpl::erase_key<
-        typename boost::mpl::reachable_set<dependency_graph, Root>::type, Root
+        typename boost::mpl::reachable_set<dependency_graph, FakeRoot>::type,
+        FakeRoot
     >::type;
 };
 }} // end namespace react::detail
 
 namespace boost { namespace mpl {
-// We need to specialize `mpl::tag_of` because `vertices` uses it while
-// `dependency_graph` is still incomplete. Because of this, it appears
-// that `tag_of` can't fetch the nested `mpl::tag`.
-template <typename Computations>
-struct tag_of<react::detail::dependency_graph<Computations>> {
-    using type = react::detail::dependency_graph_tag;
-};
-
-template <>
-struct at_impl<react::detail::dependency_graph_tag> {
-    template <typename Graph, typename Name>
-    struct apply
-        : Graph::template at<Name>
-    { };
+// We need to specialize `mpl::tag_of` because it fails otherwise.
+// I think it is due to `vertices` using `mpl::tag_of` while
+// `dependency_graph` is still incomplete.
+template <typename Computations, typename GetComputation>
+struct tag_of<react::detail::dependency_graph<Computations, GetComputation>> {
+    using type = react::detail::dependency_graph<Computations, GetComputation>;
 };
 
 namespace extension {
-    template <>
-    struct open_neighborhood_of_impl<react::detail::dependency_graph_tag> {
+    template <typename Computations, typename GetComputation>
+    struct open_neighborhood_of_impl<
+        react::detail::dependency_graph<Computations, GetComputation>
+    > {
         template <typename Graph, typename Computation>
         struct apply {
             using type = transform_view<
                 typename react::dependencies_of<Computation>::type,
-                at<Graph, _1>
+                apply_wrap1<typename lambda<GetComputation>::type, _1>
             >;
         };
     };
