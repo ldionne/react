ccd3ec905ec5ad98d8915dde5a1e9be3687350a0
Louis Dionne <ldionne.2@gmail.com>
2013-08-14 14:05:52 -0400

    Add the name of the computation when failing in `extern_`.

--- include/react/computation/extern.hpp
+++ include/react/computation/extern.hpp
@@ -9,18 +9,28 @@
 #include <react/computation/named.hpp>
 #include <react/detail/dont_care.hpp>
 
+#include <boost/mpl/assert.hpp>
+
 
 namespace react { namespace computation {
     template <typename ComputationName>
     struct extern_ : named<ComputationName> {
         template <bool always_false = false>
         static void execute(detail::dont_care, detail::dont_care) {
+            BOOST_MPL_ASSERT_MSG(always_false,
+                trying_to_execute_an_extern_computation,
+                (ComputationName)
+            );
             static_assert(always_false,
                 "trying to execute an `extern_` computation");
         }
 
         template <bool always_false = false>
         static void retrieve(detail::dont_care, detail::dont_care) {
+            BOOST_MPL_ASSERT_MSG(always_false,
+                trying_to_retrieve_the_result_of_an_extern_computation,
+                (ComputationName)
+            );
             static_assert(always_false,
                 "trying to retrieve the result of an `extern_` computation");
         }
