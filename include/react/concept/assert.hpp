18c3088108d1ce90d376c625d713523cc6c98797
Louis Dionne <ldionne.2@gmail.com>
2013-09-22 18:22:12 -0400

    Silence warning from gcc with CONCEPT_ASSERT.

--- include/react/concept/assert.hpp
+++ include/react/concept/assert.hpp
@@ -7,19 +7,29 @@
 #define REACT_CONCEPT_ASSERT_HPP
 
 namespace react {
+    namespace concept_assert_detail {
+        template <void (*)()>
+        struct always_true {
+            static constexpr bool value = true;
+        };
+    }
+
     template <typename Concept>
-    struct CONCEPT_CHECKING_FAILED_FOR {
-        static void xxxxxxxxxxxxxxxxxxxx() {
+    class CONCEPT_CHECKING_FAILED_FOR {
+        static void instantiate_destructor() {
             ((Concept*)nullptr)->~Concept();
         }
+
+    public:
+        static constexpr bool value = concept_assert_detail::always_true<
+            &CONCEPT_CHECKING_FAILED_FOR::instantiate_destructor
+        >::value;
     };
 } // end namespace react
 
 #define REACT_CONCEPT_ASSERT(... /* concept checking class */)              \
     static_assert(                                                          \
-        &::react::CONCEPT_CHECKING_FAILED_FOR<                              \
-            __VA_ARGS__                                                     \
-        >::xxxxxxxxxxxxxxxxxxxx,                                            \
+        ::react::CONCEPT_CHECKING_FAILED_FOR<__VA_ARGS__>::value,           \
         "Concept check failed: " #__VA_ARGS__                               \
     )                                                                       \
 /**/
